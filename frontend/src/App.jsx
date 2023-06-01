fe52128b0bc0d2dd2447093739e98e776343db83
ealpha072 <ealpha072@gmail.com>
2023-06-01 11:25:00 +0300

    Eslint rules and changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,19 @@
-import './App.css'
+import "./App.css";
 
-function App() {
-
-  return (
-    <>
-      <div className='first-container'>
-        <div className="left"></div>
-        <div className="right">
-          <div className="header">
-            <header>
-              
-            </header>
-          </div>
-        </div>
-        <div className="image"></div>
-        
-      </div>
-    </>
-  )
+const App = () => {
+    return (
+        <>
+            <div className="first-container">
+                <div className="left"></div>
+                <div className="right">
+                    <div className="header">
+                        <header></header>
+                    </div>
+                </div>
+                <div className="image"></div>
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
