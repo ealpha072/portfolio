83709176a9dffc566faa3fa3450c34033aaed5b3
ealpha072 <ealpha072@gmail.com>
2023-06-03 13:30:00 +0300

    Initial page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,28 @@
 import "./App.css";
+import headerStyles from "../src/styles/Header.module.css"
 
 const App = () => {
     return (
         <>
-            <div className="first-container">
-                <div className="left"></div>
-                <div className="right">
-                    <div className="header">
-                        <header></header>
+            <div >
+                <nav className={headerStyles.mainHedaer }>
+                    <div>
+                        <div>Experience</div>
+                        <div>Work</div>
+                        <div>Photography</div>
+                        <div>Contacts</div>
+                    </div>
+                </nav>
+
+                <div className="secondDiv">
+                    <div className="imageDiv"></div>
+                    <div className="textDiv">
+                        <h1>I am Alpha </h1>
+                        <h1>Emmanuell</h1>
+                        <h1>A fullstack developer</h1>
+                        <h1>Based in Kenya</h1>
                     </div>
                 </div>
-                <div className="image"></div>
             </div>
         </>
     );
