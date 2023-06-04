91c93458064e9ea4f15b5baa5fb99ec4c975d0ad
ealpha072 <ealpha072@gmail.com>
2023-06-04 15:30:00 +0300

    More pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,6 +23,52 @@ const App = () => {
                         <h1>Based in Kenya</h1>
                     </div>
                 </div>
+
+                <div className="workExperience">
+                    <div className="headings">
+                        <h1>Work Experience</h1>
+                        <h1>Companies i have worked for in the past</h1>
+                    </div>
+                    <div className="companies">
+                        <div className="company">
+                            <h1>01</h1>
+                            <h2>Google, Interaction <br /> Designer</h2>
+
+                            <p>Cuurently working here as a product designer</p>
+                        </div>
+                        <div className="company">
+                            <h1>01</h1>
+                            <h2>Google, Interaction <br /> Designer</h2>
+
+                            <p>Cuurently working here as a product designer</p>
+                        </div>
+                        <div className="company">
+                            <h1>01</h1>
+                            <h2>Google, Interaction <br /> Designer</h2>
+
+                            <p>Cuurently working here as a product designer</p>
+                        </div>
+                    </div>
+
+                    <div className="philosophy">
+                        <div className="lleft">
+                            <h1>
+                                Philosophy & values
+                            </h1>
+                            <p>
+                                I think everyone wants the same thing - 
+                                relationship with humanity, peace with the 
+                                metaphysical, and experience with the universe. 
+                                I try to grasp these things with my values: 
+                                authenticity, creativity, & hospitality.
+                            </p>
+                        </div>
+                        <div className="right">
+                            To contain image
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </>
     );
